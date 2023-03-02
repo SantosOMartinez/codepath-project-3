3a5505a4c4b71df5a675d9b69bec7fa26015af9a
Santos O. Martínez Braña <martinezsantos3@gmail.com>
2023-03-01 20:22:46 -0400

    Initial commit to main

---
+++ Project 2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project 2
+//
+//  Created by Santos O. Martínez Braña on 3/1/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
